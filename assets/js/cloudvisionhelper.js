7352d74ca7d760907a52746784738c4b61feb990
Abram Thau <47679905+Glacian22@users.noreply.github.com>
2019-03-19 19:34:02 -0700

    created trigger for label buttons (#42)
    * created trigger for label buttons
    
    * better comments

--- assets/js/cloudvisionhelper.js
+++ assets/js/cloudvisionhelper.js
@@ -5,61 +5,60 @@
 // ***REQUIRES LIVE SERVER WHEN IN TEST ENVIRONMENT***
 
 var search = {
-    "requests": [
-        {
-            "image": {
-                "source": {
-                    "imageUri":
-                        ""
-                }
-            },
-            "features": [
-                {
-                    "type": "LABEL_DETECTION",
-                    "maxResults": 5
-                }
-            ]
+  'requests': [
+    {
+      'image': {
+        'source': {
+          'imageUri':
+                        ''
         }
-    ]
+      },
+      'features': [
+        {
+          'type': 'LABEL_DETECTION',
+          'maxResults': 5
+        }
+      ]
+    }
+  ]
 }
 
-//takes in a URL in the form of a string, and max number of results 
-//returns response object
-function visionByURL(url, numResults) {
-    search.requests[0].image.source.imageUri = url;
-    search.requests[0].features[0].maxResults = numResults;
+// takes in a URL in the form of a string, and max number of results
+// returns response object
+function visionByURL (url, numResults) {
+  search.requests[0].image.source.imageUri = url
+  search.requests[0].features[0].maxResults = numResults
 
-    // 1. Load the JavaScript client library.
-    return gapi.load('client', start);
+  // 1. Load the JavaScript client library.
+  return gapi.load('client', start)
 }
 
-function start() {
-    // 2. Initialize the JavaScript client library.
-    return gapi.client.init({
-        'apiKey': 'AIzaSyB5sUYWoVsXdrrobdoe9u2NUiTP-QrbuQU',
+function start () {
+  // 2. Initialize the JavaScript client library.
+  return gapi.client.init({
+    'apiKey': 'AIzaSyB5sUYWoVsXdrrobdoe9u2NUiTP-QrbuQU'
+  })
+    .then(function () {
+      // 3. Initialize and make the API request.
+      return gapi.client.request({
+        'path': 'https://vision.googleapis.com/v1/images:annotate?',
+        'method': 'POST',
+        'body': search
+      })
     })
-        .then(function () {
-            // 3. Initialize and make the API request.
-            return gapi.client.request({
-                'path': 'https://vision.googleapis.com/v1/images:annotate?',
-                'method': 'POST',
-                'body': search
-            })
-        })
-        .then(function (response) {
-            var labels = [];
-            var rawLabels = response.result.responses[0].labelAnnotations;
+    .then(function (response) {
+      var labels = []
+      var rawLabels = response.result.responses[0].labelAnnotations
 
-            for (i = 0; i < rawLabels.length; i++) {
-                labels[i] = rawLabels[i].description;
-            }
-            console.log("Labels array: ", labels)
+      for (i = 0; i < rawLabels.length; i++) {
+        labels[i] = rawLabels[i].description
+      }
+      console.log('Labels array: ', labels)
 
-            //TODO: make wikipedia API call based on "response"
-        }, function (reason) {
-            console.log('Error: ' + reason.result.error.message);
-        });
+      // TODO: make wikipedia API call based on "response"
+    }, function (reason) {
+      console.log('Error: ' + reason.result.error.message)
+    })
 };
 
 // sample: visionByURL("http://imgURL...", 5);
-
